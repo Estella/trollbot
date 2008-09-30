876a4da458ee0d205f56f46f93961a8c6e227d67
acidtoken <acidtoken@79ca4b1d-1696-da11-8a5f-000b6a37957f>
2008-09-30 07:33:41 +0000

    left out a file
    git-svn-id: https://trollbot.svn.sourceforge.net/svnroot/trollbot/trunk@166 79ca4b1d-1696-da11-8a5f-000b6a37957f

---
+++ scripts_dist/ics_test.tcl
@@ -0,0 +1,56 @@
+ics_bind notify  - "*tehcheckersking*" follow_tehcheckersking
+ics_bind notify  - "*saldeeznuts*"     follow_sal
+ics_bind connect - "*"                 initiate_stalking
+ics_bind game    - "*"                 announce_game
+ics_bind move    - "*"                 announce_move
+
+# changes are flag changes
+# flags are as follows:
+# 
+#  * pawn_double_push_file
+#  * white_can_castle_short
+#  * black_can_castle_short
+#  * white_can_castle_long
+#  * black_can_castle_long
+#  * last_irreversible_move
+# 
+# board is a list with lists by rank
+# [lindex $board 0] = 1st rank
+proc announce_move { ics game_id move_id } {
+	
+}
+
+proc announce_game { ics game_id white black start_time time_increment } {
+	irc_interp "DALnet" "puthelp \"PRIVMSG #php :(Game:$game_id) New FICS game started. White is played by $white and black by $black.\""
+}
+
+
+proc initiate_stalking { ics who } {
+	putics $ics "+notify saldeeznuts"
+	putics $ics "+notify tehcheckersking"
+	putics $ics "follow tehcheckersking"
+	putics $ics "follow saldeeznuts"
+}
+
+proc follow_tehcheckersking { ics who action } {
+	if {$action == "arrived."} {
+		irc_interp "DALnet" "puthelp \"PRIVMSG #php :tehcheckersking (poutine) has been spotted on freechess. Now following his games.\""
+		putics $ics "follow tehcheckersking"
+	} elseif {$action == "departed."} {
+		irc_interp "DALnet" "puthelp \"PRIVMSG #php :tehcheckersking (poutine) has left freechess.\""
+	}
+
+	return
+}
+
+proc follow_sal { ics who action } {
+	if {$action == "arrived."} {
+		irc_interp "DALnet" "puthelp \"PRIVMSG #christian_debate :Saldeeznuts (sal_dz) has been spotted on freechess. Starting to stalk that greasy bastard.\""
+		putics $ics "follow saldeeznuts"
+	} elseif {$action == "departed."} {
+		irc_interp "DALnet" "puthelp \"PRIVMSG #php :saldeeznuts (sal_dz) has left freechess.\""
+	}
+
+}
+
+puts "FICS Notify Stalker Loaded."
